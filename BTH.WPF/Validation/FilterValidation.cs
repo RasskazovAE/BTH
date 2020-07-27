6d0e8d31f42d91e56bb4ea3d4914d3ee4c6d008c
RasskazovOE <mr.rasskazovoleg@gmail.com>
2020-07-27 19:55:13 +0300

    Validating StartDate and EndDate on the picker tabs

---
+++ BTH.WPF/Validation/FilterValidation.cs
@@ -0,0 +1,81 @@
+﻿using BTH.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BTH.WPF.Validation
+{
+    public class EndDateValidationRule : ValidationRule
+    {
+        public FilterWrapper FilterWrapper { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            DateTime? date = (DateTime?)value;
+            if (FilterWrapper.Filter.StartDate != null)
+                return new ValidationResult(date >= FilterWrapper.Filter.StartDate, "");
+
+            return ValidationResult.ValidResult;
+        }
+    }
+    public class StartDateValidationRule : ValidationRule
+    {
+        public FilterWrapper FilterWrapper { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            DateTime? date = (DateTime?)value;
+
+            if (FilterWrapper.Filter.EndDate != null)
+                return new ValidationResult(date <= FilterWrapper.Filter.EndDate, "");
+
+            return ValidationResult.ValidResult;
+        }
+    }
+
+
+    public class FilterWrapper : DependencyObject
+    {
+        public static readonly DependencyProperty StartDateProperty = DependencyProperty.Register(
+            "Filter",
+            typeof(Filter),
+            typeof(FilterWrapper),
+            new PropertyMetadata(default(object)));
+
+        public Filter Filter
+        {
+            get
+            {
+                return (Filter)GetValue(StartDateProperty);
+            }
+            set
+            {
+                SetValue(StartDateProperty, value);
+            }
+        }
+    }
+
+    public class BindingProxy : Freezable
+    {
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        public object Data
+        {
+            get
+            {
+                return (object)GetValue(DataProperty);
+            }
+            set
+            {
+                SetValue(DataProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new PropertyMetadata(null));
+    }
+}
--- BTH.WPF/Views/CoBaTransactionsView.xaml.cs
+++ BTH.WPF/Views/CoBaTransactionsView.xaml.cs
@@ -1,4 +1,10 @@
-﻿using MvvmCross.Platforms.Wpf.Views;
+﻿using BHT.Core.Readers.CoBa;
+using BHT.Core.Services.CoBa;
+using BTH.Core.Dto;
+using BTH.Core.ViewModels;
+using BTH.Core.ViewModels.Interfaces;
+using BTH.IoC;
+using MvvmCross.Platforms.Wpf.Views;
 
 namespace BTH.WPF.Views
 {
@@ -10,6 +16,11 @@ namespace BTH.WPF.Views
         public CoBaTransactionsView()
         {
             InitializeComponent();
+            this.DataContext = new CoBaTransactionsViewModel(
+                Manager.Instance.Container.Resolve<IFileDialogExplorer>(),
+                Manager.Instance.Container.Resolve<ICoBaReader>(),
+                Manager.Instance.Container.Resolve<ICoBaService>()
+                ) ;
         }
     }
 }
